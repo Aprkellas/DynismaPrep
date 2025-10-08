632f86814657d6ada17e5f101d4263f88d6bc7a4
Alex Kellas <alex.kellas.relton@gmail.com>
2025-10-08 14:48:11 +0100

    parser file made

---
+++ TelemetryParser/parser.py
@@ -0,0 +1,11 @@
+import json 
+import pandas as pd
+
+def parse_telemetry(file: str):
+    with open(file, 'r') as f:
+        data = json.load(f)
+    
+    df = pd.json_normalize(data)
+    return df
+
+# extend with other parsing functions as needed
