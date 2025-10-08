7875a35aa9e8ac65d0e493ac2d3278789af0df1d
Alex Kellas <alex.kellas.relton@gmail.com>
2025-10-08 15:00:01 +0100

    parsing fixed and main created

--- TelemetryParser/main.py
+++ TelemetryParser/main.py
@@ -1,4 +1,11 @@
 from parser import parse_telemetry
 
+def main():
+    file_path = input("Enter the path to the telemetry file: ")
 
-    
+    df = parse_telemetry(file_path)
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()b 
--- TelemetryParser/parser.py
+++ TelemetryParser/parser.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def parse_telemetry(file: str):
     with open(file, 'r') as f:
-        data = json.load(f)
+        data = [json.loads(line) for line in f]
     
     df = pd.json_normalize(data)
     return df
