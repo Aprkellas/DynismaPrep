cd44d603649586e4f0e26269fb0974147a82e865
Alex Kellas <alex.kellas.relton@gmail.com>
2025-10-23 14:47:27 +0100

    pandas install

--- MochInterview/Task_2/task_2.py
+++ MochInterview/Task_2/task_2.py
@@ -1,11 +1,23 @@
-
+from time import sleep
+import pandas as pd
 
 
 class SimTimer:
-    def __init__(self, df, playback_rate=1.0):
+    def __init__(self, df: pd.DataFrame, playback_rate=1.0):
         self.df = df
         self.playback_rate = playback_rate
+        self.running = False
+        self.index = 0
 
-    def start(self):
+    async def start(self):
         """Stream each row at real-time intervals"""
-        ...
+        self.running = True
+        while self.running:
+            row = self.df.iloc[self.index]
+            print(row)
+            self.index += 1
+            sleep(self.playback_rate)
+
+
+    def stop(self):
+        self.running = False
