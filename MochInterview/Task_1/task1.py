a1b1f28667215a9eeeb2a0e9547fa7ae7256e590
Alex Kellas <alex.kellas.relton@gmail.com>
2025-10-23 13:09:17 +0100

    moving avg calculated

--- MochInterview/Task_1/task1.py
+++ MochInterview/Task_1/task1.py
@@ -16,10 +16,11 @@ class TimeSeries:
 
 class Ingest:
     def __init__(self, buffer: int = 10):
-        self.series: List[TimeSeries] = []
+        self.series: dict[datetime, TimeSeries] = []
+        self.moving_averages: dict[datetime, float] = []
         self.buffer = buffer
 
-    def ingest(self, data: dict) -> TimeSeries:
+    def ingest(self, data: dict) -> bool:
         series = TimeSeries(
             timestamp=data["timestamp"],
             accel_x=data["accel_x"],
@@ -29,13 +30,31 @@ class Ingest:
             gyro_y=data["gyro_y"],
             gyro_z=data["gyro_z"],
         ) 
-        self.add_series(series)
-        return series
+        if(self.add_series(series)):
+            if (self.compute_moving_average(series.timestamp)):
+                return True
+        return False
     
-    def add_series(self, series: TimeSeries) -> None:
-        self.series.append(series)
-        if len(self.series) > self.buffer:
-            self.series.pop(0)
+    def add_series(self, series: TimeSeries) -> bool:
+        try:
+            self.series[series.timestamp] = series
+            if len(self.series) > self.buffer:
+                self.series.pop(next(iter(self.series)))
+            return True
+        except:
+            return False
+
+    def compute_moving_average(self, key: datetime) -> bool:
+        val = self.series[key]
+        if val is None:
+            return False
+        
+        total = (val.accel_x + val.accel_y + val.accel_z)
+        if (total == 0):
+            return False
+        
+        self.moving_averages[key] = total / 3
+        return True
 
     def get_series(self) -> List[TimeSeries]:
         return self.series
